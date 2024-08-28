c333526215e845ea631d8e415e1bafa8d04c45ed
qoraudrb <qoraudrb123@gmail.com>
2024-08-28 19:13:31 +0900

    Initial commit

---
+++ src/main/java/skkunion/union2024/Skkunion2024Application.java
@@ -0,0 +1,13 @@
+package skkunion.union2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skkunion2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skkunion2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/skkunion/union2024/Skkunion2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package skkunion.union2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skkunion2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
